960776b4fc90f3f2bb21a1e2bcdd14ea840ca0fd
Andrea Maldonado <>
2023-03-27 15:57:57 +0200

    Makes meta_feature_extraction pip installable

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="meta_feature_extraction",
+    version="0.0.1",
+    description="Make meta-learning applied to process mining a package",
+    packages=setuptools.find_packages(),
+    python_requires='>=3',
+)
